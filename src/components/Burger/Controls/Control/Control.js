7ab18d916db8474a803f8f59a3766acbc9e00419
Rodrigo <rodrake@gmail.com>
2020-03-31 23:33:22 -0300

    131. Adding the Build Control Component

---
+++ src/components/Burger/Controls/Control/Control.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Control.module.css';
+
+const control = props => (
+  <div className={classes.Control}>
+    <div className={classes.Label}>{props.ingredient}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
+  </div>
+);
+
+export default control;
---
+++ src/components/Burger/Controls/Controls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Controls.module.css';
+
+const controls = props = (
+  <div className={classes.Controls}>
+  
+  </div>
+);
+
+export default controls;
