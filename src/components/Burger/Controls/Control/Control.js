fb4de4e7e698211144e541c4208e019baa0cffcc
Rodrigo <rodrake@gmail.com>
2020-03-31 23:44:25 -0300

    132. Outputting Multiple Build Controls

--- src/components/Burger/Controls/Control/Control.js
+++ src/components/Burger/Controls/Control/Control.js
@@ -3,7 +3,7 @@ import classes from './Control.module.css';
 
 const control = props => (
   <div className={classes.Control}>
-    <div className={classes.Label}>{props.ingredient}</div>
+    <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
     <button className={classes.More}>More</button>
   </div>
--- src/components/Burger/Controls/Controls.js
+++ src/components/Burger/Controls/Controls.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Control from './Control/Control';
 import classes from './Controls.module.css';
 
-const controls = props = (
+const initialControls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const controls = props => (
   <div className={classes.Controls}>
-  
+    {initialControls.map(control => {
+      return <Control key={control.label} label={control.label} />
+    })}
   </div>
 );
 
