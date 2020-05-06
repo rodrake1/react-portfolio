2170f30c2e4daea96c62ef5cc015e9b534185d14
Rodrigo <rodrake@gmail.com>
2020-05-05 23:59:23 -0300

    151. Adding a Hamburger Icon

--- src/components/Navigation/SideMenu/SideMenuButton/SideMenuButton.js
+++ src/components/Navigation/SideMenu/SideMenuButton/SideMenuButton.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import classes from './SideMenuButton.module.css';
 
 const sideMenuButton = props => (
-  <div onClick={props.onOpen}>
-    MENU
+  <div
+    className={classes.SideMenuButton}
+    onClick={props.onOpen}
+  >
+    <div />
+    <div />
+    <div />
   </div>
 );
 
